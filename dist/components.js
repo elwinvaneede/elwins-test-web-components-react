09f7abae8a03a7fef29f525d3974b5308f7562c2
Elwin van Eede <elwinvaneede@gmail.com>
2020-02-16 14:42:31 +0100

    First build

---
+++ dist/components.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+import { defineCustomElements, applyPolyfills } from 'elwins-test-web-components/loader';
+applyPolyfills().then(() => defineCustomElements());
+export const MyComponent = /*@__PURE__*/ createReactComponent('my-component');
+//# sourceMappingURL=components.js.map
