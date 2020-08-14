169cc6317c446588a852515a214475b89f81f714
Elwin van Eede <elwinvaneede@gmail.com>
2020-08-14 19:26:43 +0200

    New build from the core package

--- src/components.ts
+++ src/components.ts
@@ -3,10 +3,9 @@
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
 
-import { JSX } from 'elwins-test-web-components';
+import type { JSX } from 'elwins-test-web-components';
 
-import { defineCustomElements, applyPolyfills } from 'elwins-test-web-components/loader';
 
-applyPolyfills().then(() => defineCustomElements());
+
 export const EveButton = /*@__PURE__*/createReactComponent<JSX.EveButton, HTMLEveButtonElement>('eve-button');
 export const EveIntroduction = /*@__PURE__*/createReactComponent<JSX.EveIntroduction, HTMLEveIntroductionElement>('eve-introduction');
